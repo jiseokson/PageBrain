906bd0b5806d04c3eab943043354ec60b87d0284
Jiseok Son <jiseokson@gmail.com>
2025-11-13 23:26:57 +0900

    test: add CacheManager write and read tests

---
+++ test/test_cache_manager.py
@@ -0,0 +1,64 @@
+import logging
+import random
+import uuid
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+from pagebrain.block import BlockManager
+from pagebrain.cache import CacheManager
+from utils import make_dummy_input_cache_pos, make_dummy_keys_values
+
+
+device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
+model_name = 'openai-community/gpt2'
+
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+tokenizer.pad_token = tokenizer.eos_token
+
+model = AutoModelForCausalLM.from_pretrained(model_name)
+model.to(device)
+model.eval()
+
+num_heads = model.config.n_head
+num_layers = model.config.n_layer
+d_head = model.config.n_embd // num_heads
+kv_dtype = torch.float32
+
+
+def test_write_and_read():
+  RANDOM_SEED = 42
+  random.seed(RANDOM_SEED)
+  torch.manual_seed(RANDOM_SEED)
+  torch.cuda.manual_seed_all(RANDOM_SEED)
+  
+  num_blocks = 1000
+  batch_size = 10
+  max_len = 100
+  page_size = 32
+
+  block_manager = BlockManager(num_blocks, num_layers, num_heads, d_head, page_size, device, dtype=kv_dtype)
+  cache_manager = CacheManager(block_manager)
+
+  layer_idx = 0
+  seq_ids = [str(uuid.uuid4().hex) for _ in range(batch_size)]
+  input_pos, cache_pos = make_dummy_input_cache_pos(batch_size, max_len, device, dtype=torch.int)
+  max_cache_len = torch.max(cache_pos[:, 1]).item()
+  keys, values = make_dummy_keys_values(batch_size, num_heads, max_cache_len, d_head, device=device, dtype=kv_dtype)
+
+  cache_manager.update(seq_ids, layer_idx, keys, values, input_pos)
+
+  read_keys, read_values = [], []
+  for k_p, v_p in cache_manager.iter_pages(seq_ids, layer_idx, cache_pos):
+    # k_p, v_p: [B, H, P, D]
+    read_keys.append(k_p)
+    read_values.append(v_p)
+  read_keys = torch.cat(read_keys, dim=2) # [B, H, n*P, D]
+  read_values = torch.cat(read_values, dim=2) # [B, H, n*P, D]
+
+  cache_len = cache_pos[:, 1].tolist()
+
+  for orig_key, read_key, clen in zip(keys, read_keys, cache_len):
+    assert torch.isclose(orig_key[:, :clen, :], read_key[:, :clen, :])
+
+  for orig_value, read_value, clen in zip(values, read_values, cache_len):
+    assert torch.isclose(orig_value[:, :clen, :], read_value[:, :clen, :])
---
+++ test/utils.py
@@ -0,0 +1,39 @@
+import random
+
+import torch
+
+
+def make_dummy_input_cache_pos(batch_size, max_len, device, dtype):
+  input_pos = torch.zeros(batch_size, 2, device=device, dtype=dtype)
+  cache_pos = torch.zeros(batch_size, 2, device=device, dtype=dtype)
+
+  # 50 : 40 : 10 = [0, len] : [start, 1] : [start, len]
+  for sample_idx in range(batch_size):
+    if random.randint(0, 1):
+      # start = 0
+      input_pos[sample_idx, 0] = 0
+      input_pos[sample_idx, 1] = random.randint(1, max_len)
+    else:
+      # start != 0
+      if random.randint(0, 4):
+        # len == 1
+        input_pos[sample_idx, 0] = random.randint(0, max_len-1)
+        input_pos[sample_idx, 1] = 1
+      else:
+        # len > 1
+        a, b = random.randint(0, max_len-1), random.randint(0, max_len-1)
+        a, b = min(a, b), max(a, b)
+        start = a
+        length = b - a + 1
+        input_pos[sample_idx, 0] = start
+        input_pos[sample_idx, 1] = length
+
+  cache_pos[:, 1] = input_pos[:, 0]
+
+  return input_pos, cache_pos
+
+
+def make_dummy_keys_values(batch_size, num_heads, seq_len, d_head, device, dtype):
+  keys = torch.rand([batch_size, num_heads, seq_len, d_head], device=device, dtype=dtype)
+  values = torch.rand([batch_size, num_heads, seq_len, d_head], device=device, dtype=dtype)
+  return keys, values
