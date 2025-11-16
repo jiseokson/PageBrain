3e46c5f9ef2ac4d4350d122c8835ceed9d1be92a
Jiseok Son <jiseokson@gmail.com>
2025-11-17 03:51:40 +0900

    feat: implement SeqGroup creation and initialization

--- src/pagebrain/sequence.py
+++ src/pagebrain/sequence.py
@@ -1,6 +1,8 @@
 import asyncio
-from typing import Literal, Optional
+from typing import List, Literal, Optional
 import uuid
+
+import torch
 
 
 SeqId = str
@@ -29,8 +31,42 @@ class Sequence:
     self.new_token = False
     self.done = False
 
-    # self.cache_pos
-    # self.input_pos
-    # self.token_ids
-    # self.num_gen_tokens
-    self.token_text: str
+    self.token_buffer: List[int] = []
+    self.cache_start: int = 0
+    self.cache_len: int = 0
+    self.input_start: int = 0
+    self.input_len: int = 0
+    self.gen_tokens: List[str] = []
+
+
+class SequenceGroup:
+  def __init__(self, seqs: List[Sequence], device):
+    self.seqs = seqs
+    self.device = device
+
+    self.input_ids: torch.Tensor
+    self.seq_ids: List[SeqId] = [seq.id for seq in seqs]
+
+    self.cache_pos: torch.Tensor
+    self.input_pos: torch.Tensor
+
+    cache_starts = [seq.cache_start for seq in seqs]
+    cache_lens = [seq.cache_len for seq in seqs]
+    input_starts = [seq.input_start for seq in seqs]
+    input_lens = [seq.input_len for seq in seqs]
+
+    self.cache_pos = torch.tensor(
+      [cache_starts, cache_lens], device=device, dtype=torch.long
+    ).transpose(0, 1).contiguous()
+    self.input_pos = torch.tensor(
+      [input_starts, input_lens], device=device, dtype=torch.long
+    ).transpose(0, 1).contiguous()
+
+    batch_size = len(seqs)
+    input_width = max(input_lens)
+    self.input_ids = torch.zeros([batch_size, input_width], device=device, dtype=torch.long)
+    for sample_idx, seq in enumerate(seqs):
+      self.input_ids[sample_idx, :seq.input_len] = torch.tensor(seq.token_buffer[:seq.input_len])
+
+  def update(self, next_token_ids: torch.Tensor):
+    pass
---
+++ tests/test_sequence.py
@@ -0,0 +1,86 @@
+import asyncio
+import logging
+import random
+
+import torch
+from transformers import AutoTokenizer
+from pagebrain.engine import Engine
+from pagebrain.sequence import Sequence, SequenceGroup
+from utils import make_engine_reqs, set_random_seed
+
+
+logger = logging.getLogger(__name__)
+
+
+device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
+model_name = 'openai-community/gpt2'
+
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+tokenizer.pad_token = tokenizer.eos_token
+
+
+prompts = [
+  'Once upon a time, there was a',
+  'In the future, AI will',
+  'The meaning of life is',
+  'FastAPI is a great framework for',
+  'Transformers models are powerful for',
+  'It was a sunny day when',
+  'Quantum computing will change',
+  'The secret to happiness is',
+  'Long ago in a galaxy far',
+  'Python is the best language for',
+]
+
+
+def test_sequence_group(use_seed):
+  if use_seed:
+    set_random_seed(42)
+
+  engine_reqs = make_engine_reqs(prompts)
+
+  seqs = []
+  for engine_req in engine_reqs:
+    seq = Sequence(
+      engine_req.prompt,
+      engine_req.max_new_tokens,
+      engine_req.method,
+      engine_req.temperature,
+      engine_req.top_p,
+      engine_req.top_k,
+      asyncio.Event(),
+    )
+    seqs.append(seq)
+
+  engine = Engine(model_name=model_name, device=device)
+  # Fill the token_buffer using the function below
+  # Tests for this function are written in test_engine.py
+  engine._init_batch_sequence_before_sched(seqs)
+
+  # Simulate a situation where the scheduler determines the prefill forward length
+  cache_starts = []
+  cache_lens = []
+  input_starts = []
+  input_lens = []
+  for seq in seqs:
+    tokens = len(seq.token_buffer)
+    seq.cache_start = 0
+    seq.cache_len = random.randint(0, tokens-1)
+    seq.input_start = seq.cache_len
+    seq.input_len = tokens - seq.input_start
+
+    cache_starts.append(seq.cache_start)
+    cache_lens.append(seq.cache_len)
+    input_starts.append(seq.input_start)
+    input_lens.append(seq.input_len)
+
+  seq_group = SequenceGroup(seqs, device=device)
+  assert (seq_group.cache_pos[:, 0] == torch.tensor(cache_starts, device=device)).all().item()
+  assert (seq_group.cache_pos[:, 1] == torch.tensor(cache_lens, device=device)).all().item()
+  assert (seq_group.input_pos[:, 0] == torch.tensor(input_starts, device=device)).all().item()
+  assert (seq_group.input_pos[:, 1] == torch.tensor(input_lens, device=device)).all().item()
+
+  # Test whether input_ids are filled up to the region determined by the prefill forward
+  for seq, input_id, (start, length) in \
+    zip(seqs, seq_group.input_ids, seq_group.input_pos.tolist()):
+    assert (input_id[:length].squeeze() == torch.tensor(seq.token_buffer[:length], device=device)).all().item()
